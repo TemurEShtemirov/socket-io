fbf4307e7d636b8a81655f8d7b5e4f71f184115d
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-01-15 22:57:11 +0500

    installed pg for psql

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -36,14 +38,17 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+});
 
-  // Example 1
+io.on("connection", (socket) => {
   socket.emit("hello", "world");
+});
 
-  // Example 2
+io.on("connection", (socket) => {
   socket.emit("hello", 1, "2", { 3: "4", 5: Buffer.from([6]) });
+});
 
-  // Example 3
+io.on("connection", (socket) => {
   socket
     .timeout(5000)
     .emit("request", { foo: "bar" }, "baz", (err, response) => {
@@ -53,19 +58,20 @@ io.on("connection", (socket) => {
         console.log(response.status); // 'ok'
       }
     });
+});
 
-  // Example 4
+io.on("connection", async (socket) => {
   try {
     const res = await socket
       .timeout(5000)
       .emitWithAck("request", { foo: "bar" }, "baz");
     console.log(res.status);
   } catch (e) {}
+});
 
-  // Example 5
-  io.emit("hello", "world");
+io.emit("hello", "world");
 
-  // Example 6
+io.on("connection", (socket) => {
   // join the room named 'some room'
   socket.join("some room");
 
@@ -78,7 +84,6 @@ io.on("connection", (socket) => {
   // leave the room
   socket.leave("some room");
 });
-
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
 });
